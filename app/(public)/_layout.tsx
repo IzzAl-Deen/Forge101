414d6539bad3bec2e3485a88c59463203ad77962
Ahmad iyrot <133695289+Ahmadiyrot@users.noreply.github.com>
2026-05-16 23:45:16 +0300

    Update _layout.tsx

--- app/(public)/_layout.tsx
+++ app/(public)/_layout.tsx
@@ -1,9 +1,14 @@
+import { useAuth } from "@/hooks/use-auth";
 import { ExerciseFiltersProvider } from "@/contexts/ExerciseFiltersContext";
 import { queryClient } from "@/lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { Stack } from "expo-router";
+import { Redirect, Stack } from "expo-router";
 
 export default function PublicLayout() {
+  const { session, isLoading } = useAuth();
+
+  if (!isLoading && session) return <Redirect href="/(private)" />;
+
   return (
     <QueryClientProvider client={queryClient}>
       <ExerciseFiltersProvider>
